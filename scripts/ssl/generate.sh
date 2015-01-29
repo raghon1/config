7164bf575afbe6ff46e8300ac3e043611495ab09
root <root@seafile.cloudwalker.no>
2015-01-29 17:41:06 +0100

    lagt til monitor skript

--- scripts/ssl/generate.sh
+++ scripts/ssl/generate.sh
@@ -3,20 +3,21 @@
 # Lag priv cert, og generer signing request
 
 dir=$(cd $(dirname $0) ; pwd)
+dir_to=/var/tmp/ssl-generate
 cd $dir
 
 lag_req() {
 	server=$1
-	mkdir -p $dir/kunder/$server
-	openssl req -config ./req.cfg -newkey rsa:2048 -nodes -keyout kunder/$server/$server.key -out kunder/$server/$server.csr
-	chmod 600 $dir/kunder/$server/*
+	mkdir -p $dir_to/kunder/$server
+	openssl req -config $dir/req.cfg -newkey rsa:2048 -nodes -keyout $dir_to/kunder/$server/$server.key -out $dir_to/kunder/$server/$server.csr
+	chmod 600 $dir_to/kunder/$server/*
 }
 
 show_req() {
 	server=$1
 	echo "trykk enter for å vise signing request. Denne kopieres inn i control panelet til startssl.com eller andre ssl tilbydere."
 	read enter
-	cat $dir/kunder/$server/$server.csr
+	cat $dir_to/kunder/$server/$server.csr
 }
 
 store_crt() {
@@ -24,7 +25,7 @@ store_crt() {
 	echo "Trykk enter for å åpne editoren vim, lim deretter inn certifikatet du har generert på startssl"
 	echo "avslutt vim ved å taste: <escape> ZZ"
 	read enter
-	vim $dir/kunder/$server/$server.crt
+	vim $dir_to/kunder/$server/$server.crt
 }
 
 while getopts "h:s:" arg; do
